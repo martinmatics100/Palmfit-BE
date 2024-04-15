ed411152eb82675a9cbe4b4714f6422b51cdc38d
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2024-04-15 23:33:49 +0100

    still setting up the palmfit backend project

---
+++ PalmFit.API/PalmFit.Domain/Entities/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalmFit.Domain.Entities
+{
+    public class Address : BaseEntity
+    {
+        public int HouseNumber { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
---
+++ PalmFit.API/PalmFit.Domain/Entities/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalmFit.Domain.Entities
+{
+    public class Food : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Details { get; set; }
+        public string Origin { get; set; }
+        public decimal Calorie { get; set; }
+        public decimal Carbohydrates { get; set; }
+        public decimal Protein { get; set; }
+        public string Fats { get; set; }
+        public string FoodClassId { get; set; }
+        public FoodClass FoodClass { get; set; }
+    }
+}
